4dc819348bb8bf85a55562299a64989f9e27717e
david <dperezfernandez@safareyes.es>
2024-11-29 10:18:15 +0100

    🎉 Initialize project

---
+++ src/main/java/main/buysphereapi/BuySphereApiApplication.java
@@ -0,0 +1,13 @@
+package main.buysphereapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuySphereApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuySphereApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/main/buysphereapi/BuySphereApiApplicationTests.java
@@ -0,0 +1,13 @@
+package main.buysphereapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuySphereApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
